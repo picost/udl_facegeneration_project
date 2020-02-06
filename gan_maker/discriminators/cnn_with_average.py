eccfbd338fcd417cb45cd4a3d178d88753f8b08f
picost <pierrecostini314@gmail.com>
2020-02-06 10:47:18 +0100

    Close #4

--- gan_maker/discriminators/cnn_with_average.py
+++ gan_maker/discriminators/cnn_with_average.py
@@ -10,11 +10,35 @@ import torch.nn as nn
 from ..utils import conv_BN as conv
 
 class Discriminator(nn.Module):
+    """A discriminator class to be applied on 2D images
+    
+    The discriminator structure is as follows:
+        
+        - The first layer is convolutional starting from 3 channels to the
+          number provided at init. No batch normalization is used.
+        - The two following layers increase the depths by a factor 2 while
+          decreasing the other dimensions by the same factor.
+        - ReLU is applied after each of these layers.
+        - The last layer is used for classification. A kernel with size 3 is
+          used with stride and padding of 1 to let the "2D size" unchanged while
+          decreasing the number of channels to 1. No batch-norm.
+        - The values in this last feature array are averages to provide the
+          output classification value. This is inspired from a previous experiment
+          in the cycle-gan exercise.
+    
+    """
 
     def __init__(self, conv_dim):
         """
         Initialize the Discriminator Module
-        :param conv_dim: The depth of the first convolutional layer
+        
+        Args:
+        -----
+        
+            conv_dim: The depth of the first convolutional layer. The depth
+                is then increased by a factor two at each layer.
+                
+                
         """
         super(Discriminator, self).__init__()
         # --- Convolutional layer (in calling order ) ---
@@ -28,8 +52,8 @@ class Discriminator(nn.Module):
         self.conv3 = conv(conv_dim * 2, conv_dim * 4, kernel_size=4, stride=2, 
                           padding=1, batch_norm=True)
         # 4 * 4 * conv_dim*4
-        self.conv4 = conv(conv_dim * 4 , conv_dim * 8,  kernel_size=2, stride=1, 
-                          padding=1, batch_norm=False)
+        self.conv_classify = conv(conv_dim * 4 , 1,  kernel_size=3, stride=1, 
+                                  padding=1, batch_norm=False)
         # 
         # --- Other layers
         self.relu = nn.ReLU()
@@ -41,5 +65,10 @@ class Discriminator(nn.Module):
         :return: Discriminator logits; the output of the neural network
         """
         # define feedforward behavior
-        
-        return x
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.conv_classify(x)
+        x = x.view(x.shape[0], -1)
+        out = x.mean(dim=1, keepdim=True)
+        return out
